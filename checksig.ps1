bcc9895a63710ef1d899a00f78e4b607adcb6688
Daniel Card <dan.card@xservus.com>
2022-06-02 12:26:10 +0100

    Update checksig.ps1

--- checksig.ps1
+++ checksig.ps1
@@ -1,5 +1,3 @@
-Get-ChildItem -Path C:\Windows\system32 -File -Recurse | try(Get-AuthenticodeSignature $_)catch{write-host "Not Signed" -ForegroundColor Red}
-
 $signcheck = Get-ChildItem -Path C:\Windows\system32\ -Include ('*.exe','*.dll','*.ps1') -File -Recurse -ErrorAction SilentlyContinue | Get-AuthenticodeSignature
 foreach ($check in $signcheck)
 {
